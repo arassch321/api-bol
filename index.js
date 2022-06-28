822e9f1f814a97e50cb1e4c85add3d14d0f9a48c
ArrasM <muhammadaras4321@gmail.com>
2022-06-28 17:56:52 +0800

    disable node_modules cache

--- index.js
+++ index.js
@@ -259,6 +259,6 @@ app.post('/api/komentar', (req, res) => {
 });
 
 // buat server nya
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 5000, function() {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
