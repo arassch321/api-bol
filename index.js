7f9b56cd2c796f761e6f086f63682c279d7771eb
ArrasM <muhammadaras4321@gmail.com>
2022-07-01 00:59:48 +0800

    cors

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var corsOptions = {
-    origin: "http://localhost:8080"
-};
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+
+// var corsOptions = {
+//     origin: "http://localhost:8080"
+// };
 
 // buat server nya
 
